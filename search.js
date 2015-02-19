b3598b0e3b630004f3206f4a3ef4a852d3286916
Kjell <kjell@leanside.com>
2015-02-19 16:28:02 -0600

    pull object meta from redis

--- search.js
+++ search.js
@@ -34,4 +34,31 @@ app.get('/:query', function(req, res) {
   })
 })
 
+app.get('/id/:id', function(req, res) {
+  var id = req.params.id
+  client.hget('object:'+~~(id/1000), id, function(err, reply) {
+    res.json(JSON.parse(reply))
+  })
+})
+
+app.get('/ids/:ids', function(req, res) {
+  var ids = req.params.ids.split(',')
+  var m = client.multi()
+  ids.forEach(function(id) { m.hget('object:'+~~(id/1000), id) })
+
+  m.exec(function(err, replies) {
+    var filter = req.query.filter
+    if(filter == undefined) return res.json(replies.map(function(meta) { return JSON.parse(meta) }))
+    filter = filter.split(',')
+    var filtered = replies.map(function(meta) {
+      if(meta == null) return
+      var json = JSON.parse(meta)
+      return filter.reduce(function(all, field) { 
+        all[field] = json[field]; return all
+      }, {})
+    })
+    return res.send(filtered)
+  })
+})
+
 app.listen(4680)
